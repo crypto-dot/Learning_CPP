20ed10dfa0b8ce35bd2f0c8f44b908e04e7db759
crypto-dot <78282234+crypto-dot@users.noreply.github.com>
2021-05-24 16:58:10 -0500

    Delete p16/Challenge8(Copy) directory

--- p16/Challenge8(Copy)/I_Printable.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Carlos Arbizu on 5/1/21.
-//
-#include "I_Printable.h"
-ostream &operator <<(ostream &ostream1, const I_Printable &obj)
-{
-obj.print(ostream1);
-return ostream1;
-}
